28cc9decb02df7699a7b98f11d2674204dad6447
Ka-GD <zoromidari@gmail.com>
2024-12-01 22:27:19 -0300

    fizemos a aba de livros doados e quero doar

--- src/pages/LivrosDoados/LivrosDoados.jsx
+++ src/pages/LivrosDoados/LivrosDoados.jsx
@@ -1,7 +1,27 @@
+import protagonista from '../../assets/protagonista.png'
+import s from './LivrosDoados.module.scss'
+
 export default function LivrosDoados(){
     return(
-        <section>
-            <h2>Esse é o componenter livros doados</h2>
+        <section className={s.livrosDoados}>
+           <h2>Livros Doados</h2>
+           {/*inicio da sessao dos livros */}
+           <section className={s.livrosCards}>
+            <section>
+                <img src={protagonista} alt="" />
+                <div>
+                    <h3>O protagonista</h3>
+                    <p>Susane Andrade</p>
+                    <p>Ficção</p>  
+                </div>
+        
+            </section>
+           </section>
+           {/*fim da sessão dos livros */}
+            
+           
         </section>
+        
     )
 }
+
--- src/pages/QueroDoar/QueroDoar.jsx
+++ src/pages/QueroDoar/QueroDoar.jsx
@@ -1,7 +1,25 @@
+import formlivro from '../../assets/formlivro.png'
+import s from './QueroDoar.module.scss'
 export default function QueroDoar(){
     return(
-        <section>
-            <h2>Esse é o componenter Quero Doar</h2>
+        <section className={s.QueroDoar}>
+           <p>Por favor,preencha o formulário com suas informações e as informações do livro.</p>
+
+           <form action="">
+
+                <div>
+                    <img src={formlivro} alt="imagem com icone de livro aberto com borda azul" />
+                    <h2>Informações do Livro</h2>
+            
+                </div>
+
+                <input type="text" name="" id="" placeholder='Titulo' />
+                <input type="text" name="" id="" placeholder='Categoria' />
+                <input type="text" name="" id="" placeholder='Autor' />
+                <input type="text" name="" id="" placeholder='Link da Imagem' />
+                <input type="submit" value="Doar" className={s.botão} />
+
+           </form>
         </section>
     )
 }
