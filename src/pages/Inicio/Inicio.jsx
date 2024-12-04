8801d26aa57f91f509106e8fe969fd0e318da5d1
Ka-GD <zoromidari@gmail.com>
2024-12-04 19:40:52 -0300

    modificando um pouco o sass

--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -12,19 +12,19 @@ export default function Inicio(){
             <section className={s.porqueDoarSection}>
                 <h2>Por que devo doar?</h2>
                 <section className={s.cardsMotivos}>
-                    <section>
+                    <section className={s.comunidade}>
                         <img src={comunidade} alt="imagem de icones de pessoas em circulo com fundo azul e desenho preto" />
                         <p>Oferece livros a quem não tem acesso, ajudando a reduzir a exclusão social.</p>
                     </section>
-                    <section>
+                    <section className={s.lendo}>
                         <img src={lendo} alt="imagem de um icone lendo livro com fundo azul e desenho preto" />
                         <p>Estimula o hábito da leitura e o aprendizado contínuo.</p>
                     </section>
-                    <section>
+                    <section className={s.luta}>
                         <img src={luta} alt="imagem de icones de pessoas com fundo azul e desenho preto" />
                         <p>Fornece conhecimento e inspiração, permitindo que indivíduos transformem suas vidas.</p>
                     </section>
-                    <section>
+                    <section className={s.justiça}>
                         <img src={balançajustiça} alt="imagem de balaça com fundo azul e desenho preto" />
                         <p>Garante que todos, independentemente de sua condição, tenham oportunidades de aprendizado.</p>
                     </section>
