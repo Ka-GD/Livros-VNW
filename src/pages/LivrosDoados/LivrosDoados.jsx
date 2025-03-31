c5563fb2ce50899c3cf37c408c81baa57481c126
Ka-GD <zoromidari@gmail.com>
2025-03-31 19:55:02 -0300

    fazendo a conexão do front end com a API

--- src/pages/LivrosDoados/LivrosDoados.jsx
+++ src/pages/LivrosDoados/LivrosDoados.jsx
@@ -1,7 +1,25 @@
 import protagonista from '../../assets/protagonista.png'
 import s from './LivrosDoados.module.scss'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
+
 
 export default function LivrosDoados(){
+    
+    // variavel array pra guardar as informaçãoes 
+
+    const [livros,setlivros]= useState([])
+
+    const puxarLivros = async()=> {
+        const resposta = await axios.get("https://api-hgmm.onrender.com/livroscadastrados")
+        setlivros(resposta.data.livros)
+        console.log(livros)
+    }
+    useEffect(()=>{
+        puxarLivros()
+    },[])
+    
     return(
         <section className={s.livrosDoados}>
            <h2>Livros Doados</h2>
--- src/pages/QueroDoar/QueroDoar.jsx
+++ src/pages/QueroDoar/QueroDoar.jsx
@@ -1,25 +1,58 @@
-import formlivro from '../../assets/formlivro.png'
-import s from './QueroDoar.module.scss'
-export default function QueroDoar(){
-    return(
+import axios from 'axios';
+import formlivro from '../../assets/formlivro.png';
+import s from './QueroDoar.module.scss';
+import { useState } from 'react';
+
+export default function QueroDoar() {
+    const [titulo, setTitulo] = useState("");
+    const [categoria, setCategoria] = useState("");
+    const [autor, setAutor] = useState("");
+    const [image_url, setImage_url] = useState("");
+
+    const capturaTitulo = (e) => {
+        setTitulo(e.target.value);
+    };
+
+    const capturaCategoria = (e) => {
+        setCategoria(e.target.value);
+    };
+
+    const capturaAutor = (e) => {
+        setAutor(e.target.value);
+    };
+
+    const capturaImg = (e) => {
+        setImage_url(e.target.value);
+    };
+
+    const envioDados = async () => {
+        const dadosParaEnviar = {
+            titulo,
+            categoria,
+            autor,
+            image_url
+        };
+
+        await axios.post("https://api-hgmm.onrender.com/doar", dadosParaEnviar);
+        alert("Obrigado, seu livro foi cadastrado com sucesso");
+    };
+
+    return (
         <section className={s.QueroDoar}>
-           <p>Por favor,preencha o formulário com suas informações e as informações do livro.</p>
+            <p>Por favor, preencha o formulário com suas informações e as informações do livro.</p>
 
-           <form action="">
-
+            <form onSubmit={(e) => e.preventDefault()}>
                 <div>
-                    <img src={formlivro} alt="imagem com icone de livro aberto com borda azul" />
+                    <img src={formlivro} alt="Imagem com ícone de livro aberto com borda azul" />
                     <h2>Informações do Livro</h2>
-            
                 </div>
 
-                <input type="text" name="" id="" placeholder='Titulo' />
-                <input type="text" name="" id="" placeholder='Categoria' />
-                <input type="text" name="" id="" placeholder='Autor' />
-                <input type="text" name="" id="" placeholder='Link da Imagem' />
-                <input type="submit" value="Doar" className={s.botão} />
-
-           </form>
+                <input type="text" placeholder="Título" onChange={capturaTitulo} required />
+                <input type="text" placeholder="Categoria" onChange={capturaCategoria} required />
+                <input type="text" placeholder="Autor" onChange={capturaAutor} required />
+                <input type="url" placeholder="Link da Imagem" onChange={capturaImg} required />
+                <input type="submit" value="Doar" className={s.botão} onClick={envioDados} />
+            </form>
         </section>
-    )
+    );
 }
